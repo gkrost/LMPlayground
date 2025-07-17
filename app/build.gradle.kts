5d0da5872e5642077bf06d48930ad8d82b202079
Gernot Krost <ID+username@users.noreply.github.com>
2025-07-17 18:02:29 +0200

    fixed build :-)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.6.10" // oder libs.versions.compose.compiler.get()
+        kotlinCompilerExtensionVersion = "1.6.10"
     }
 
     splits {
@@ -109,4 +109,11 @@ android {
         excludes += "/META-INF/AL2.0"
         excludes += "/META-INF/LGPL2.1"
     }
+}
+
+// Add missing Material dependencies to resolve resource errors
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.compose.material3:material3:1.3.2")
 }
